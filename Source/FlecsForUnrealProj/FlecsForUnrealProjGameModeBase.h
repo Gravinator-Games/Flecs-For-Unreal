b04a4ef6a4ce3f1f35e99602920ffa95f0efa681
Bent Nürnberg <bent@gravinatorgames.com>
2021-05-10 20:47:19 +0200

    created UE project

---
+++ Source/FlecsForUnrealProj/FlecsForUnrealProjGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlecsForUnrealProjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLECSFORUNREALPROJ_API AFlecsForUnrealProjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
